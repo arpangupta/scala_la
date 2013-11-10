4362de583faa55371ff97a8d63d5ed23cf5b359d
Arpan Gupta <arpangupta420@gmail.com>
2013-11-10 18:59:26 +0530

    added matrix multiplication, addition and subtraction

--- src/mat_add.scala
+++ src/mat_add.scala
@@ -2,7 +2,5 @@ def mat_add(m1 : Array[Array[Double]], m2 : Array[Array[Double]] ) = {
 var res = Array.ofDim[Double](m1.length, m1(0).length)
 for(i <- (0 until m1.length).par ; j <- (0 until m1(0).length).par){
 res(i)(j) = m1(i)(j) + m2(i)(j)
-}
-res
-}
+}res}
 
