96b68fa0963880d051a24d0bc87209109ae1ddbe
ahmedshabib <ahmed.bolwar@gmail.com>
2013-11-20 17:08:01 +0000

    final commit

--- src/super.sh
+++ src/super.sh
@@ -5,8 +5,8 @@ do
 
 	for c in 1 2 3 4 5 6 7 8 9 10
 	do
-	v=$((c * 500))
+	v=$((c * 500000))
 	echo "$i threads Input Size $v"
-	time scala -J-Xmx8g ParallelMatrix ${i} ${v} ${v} mat_add >out
+	time scala -J-Xmx8g ParallelVector ${i} ${v} vec_normalize >out
 	done
 done
